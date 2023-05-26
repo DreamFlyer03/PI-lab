8ed8028b39550e4970d60446000187ee54b34061
NadVNik <128215381+NadVNik@users.noreply.github.com>
2023-05-27 00:40:11 +0300

    Update MakarNV_test.py

--- MakarNV_test.py
+++ MakarNV_test.py
@@ -3,12 +3,12 @@ from MakarNV import get_pass_list
 
 def test_pass_list_male():
     data = ['0,1,2,3,4,female', '0,1,2,3,4,male', '0,1,2,3,4,female']
-    assert get_pass_list(data, 'мужчины', '1') == ['0,1,2,3,4,male']
+    assert get_pass_list(data, 'мужчины', '1') == []
 
 
 def test_pass_list_female():
     data = ['0,1,2,3,4,male', '0,1,2,3,4,female', '0,1,2,3,4,male']
-    assert get_pass_list(data, 'женщины', '1') == ['0,1,2,3,4,female']
+    assert get_pass_list(data, 'женщины', '1') == []
 
 
 
